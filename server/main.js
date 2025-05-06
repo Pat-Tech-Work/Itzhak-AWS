10bad2c8a2a9a445d92e2132f93f47bfdc5667b7
Pat-Tech-Work <pattech.work@gmail.com>
2025-05-06 15:45:15 +0300

    Update main.js

--- server/main.js
+++ server/main.js
@@ -12,7 +12,8 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173',
+  //origin: 'http://localhost:5173'
+  origin: 'https://itzhak-aws-vkmdh.ondigitalocean.app/',
   credentials: true, // Allows sending cookies
 }));
 app.use(express.json());
