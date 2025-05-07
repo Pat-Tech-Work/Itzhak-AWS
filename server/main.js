19b458413b528ccbe9e7e991f390dbd44a1e3322
Pat-Tech-Work <pattech.work@gmail.com>
2025-05-07 15:33:23 +0300

    Update main.js

--- server/main.js
+++ server/main.js
@@ -32,6 +32,6 @@ app.get('/', (req, res) => {
 
 // db -> models -> Repo -> Service -> Controller -> main.js
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(` Server is running on http://localhost:${PORT}`);
 });
