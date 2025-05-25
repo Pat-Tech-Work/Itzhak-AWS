298ca7fc11beec2bf27e0464f87badc4c8b9b6b9
Pat-Tech-Work <pattech.work@gmail.com>
2025-05-25 16:50:32 +0300

    Update App.jsx

--- client/react-tailwind/src/App.jsx
+++ client/react-tailwind/src/App.jsx
@@ -6,11 +6,12 @@ import Step_2 from "./Pages/Step_2";
 import Step_3 from "./Pages/Step_3";
 import Step_4 from "./Pages/Step_4";
 import SuccessPage from "./Pages/SuccessPage";
-
-import Dashboard from "./Pages/Dashboard";
 import CouponUpload from "./Services/CouponUpload";
 
+import Dashboard from "./Pages/Dashboard";
 
+import ProtectedRoute from './Services/ProtectedRoute'; // 25/5/2025
+import Login from './Services/Login'; // 25/5/2025
 
 function App() {
     return (
@@ -24,8 +25,10 @@ function App() {
                 <Route path="/step_3" element={<Step_3 />} />
                 <Route path="/step_4" element={<Step_4 />} />
                 <Route path="/success" element={<SuccessPage />} /> 
-                {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+                <Route path="/dashboard" element={<ProtectedRoute> <Dashboard /> </ProtectedRoute>} /> 
                 <Route path="/upload-coupons" element={<CouponUpload />} />
+
+               <Route path="/login" element={<Login />} /
 
             </Routes>
         </Router>
