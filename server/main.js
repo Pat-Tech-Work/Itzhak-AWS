69e2be13e247a5705d554682382bbbc0480622b6
Pat-Tech-Work <pattech.work@gmail.com>
2025-05-26 16:11:00 +0300

    Update main.js

--- server/main.js
+++ server/main.js
@@ -7,6 +7,8 @@ const orderNumberRoutes = require('./controllers/orderNumberController');
 const surveyRouter = require('./controllers/surveyController');
 const couponCodeRouter = require('./controllers/couponCodeController');
 const usedCouponRouter = require('./controllers/usedCouponController');
+const cookieParser = require('cookie-parser');
+const loginRouter = require('./controllers/loginController'); // 26/5/2025
 
 const cors = require('cors');
 
@@ -22,6 +24,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(cookieParser());
 connectDB();
 app.set('trust proxy', true);
 
@@ -29,6 +32,7 @@ app.use('/api/survey', surveyRouter);
 app.use('/api/orderNumber', orderNumberRoutes);
 app.use('/api/couponCode', couponCodeRouter);
 app.use('/api/usedCoupons', usedCouponRouter);
+app.use('/api/login', loginRouter); // 26/5/2025
 
 const PORT = process.env.PORT || 4000;
 app.get('/', (req, res) => {
