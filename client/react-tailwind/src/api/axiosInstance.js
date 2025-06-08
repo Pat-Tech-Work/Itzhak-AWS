3b5db00594e29c717b6b3847346db14b5ef77ceb
Pat-Tech-Work <pattech.work@gmail.com>
2025-06-08 15:10:51 +0300

    Update axiosInstance.js

--- client/react-tailwind/src/api/axiosInstance.js
+++ client/react-tailwind/src/api/axiosInstance.js
@@ -1,19 +1,26 @@
 // src/api/axiosInstance.js
 import axios from 'axios';
 
-// ודאי שמשתנה הסביבה VITE_API_BASE_URL מוגדר בקובץ .env שלך כך:
-// VITE_API_BASE_URL=https://itzhak-aws-vkmdh.ondigitalocean.app/itzhak-aws-server/api
+// ודאי ש־VITE_API_BASE_URL מוגדר בקובץ .env שלך
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
-// הכנסי את הטוקן בכל בקשה
-axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers['Authorization'] = `Bearer ${token}`;
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
+const axiosInstance = axios.create({
+  baseURL,
+  withCredentials: true,
 });
+
+// Interceptor להוספת הטוקן מה-localStorage לכל בקשה
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
